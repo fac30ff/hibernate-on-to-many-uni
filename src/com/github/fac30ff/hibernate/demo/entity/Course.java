1c7ab8302685a87bfc7d62e092e46136829d3d85
Igor Rudenko <ihor.rudenko@globallogic.com>
2019-05-08 16:25:31 +0300

    creaitng reviews

--- src/com/github/fac30ff/hibernate/demo/entity/Course.java
+++ src/com/github/fac30ff/hibernate/demo/entity/Course.java
@@ -1,13 +1,18 @@
 package com.github.fac30ff.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +27,9 @@ public class Course {
 	@ManyToOne(cascade= {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
 	@JoinColumn(name="instructor_id")
 	private Instructor instructor;
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="course_id")
+	private List<Review> reviews;
 	public Course() {
 	}
 	public Course(String title) {
@@ -44,6 +52,18 @@ public class Course {
 	}
 	public void setInstructor(Instructor instructor) {
 		this.instructor = instructor;
+	}
+	public List<Review> getReviews() {
+		return reviews;
+	}
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+	public void addReview(Review review) {
+		if (reviews == null) {
+			reviews = new ArrayList<>();
+		}
+		reviews.add(review);
 	}
 	@Override
 	public String toString() {
